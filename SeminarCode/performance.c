f264707bc94b0db3e141855252a151c3237dfd28
punshLineLama <lormi090@gmail.com>
2018-08-12 14:59:15 +0200

    added some comments

--- SeminarCode/performance.c
+++ SeminarCode/performance.c
@@ -2,6 +2,7 @@
 #include <time.h>
 
 int i=0;
+// int *i;
 long long nbIterations = 2000000000;
 
 long timediff(clock_t t1, clock_t t2) {
@@ -28,14 +29,16 @@ int inc3(int i){
 int main(){
 	clock_t t1, t2;
 	long elapsed;
-	//int *i;
-	int h = 0;
+	//int h = 0;
 	//i = &h;
 	
 	for(int k = 0; k<100; k++){
 		t1=clock();
 		for(int j = 0; j<nbIterations; j++){
 			i = inc3(i);	
+			//i++
+			//inc1()
+			//inc2(i) need to make i a pointer and let it point to h, like in
 		}
 		t2=clock();
 		elapsed = timediff(t1,t2);
